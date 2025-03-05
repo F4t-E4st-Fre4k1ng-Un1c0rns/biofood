63df179ed0ad4a23b53ef6a9ee6b49e3ceb8c0af
BeaverNotACat <beavernotacat@gmail.com>
2025-03-05 12:50:36 +1000

    🚑 Make tackeut time nullable

--- backend/src/backend/application/orders/subscript_to_order_updates.py
+++ backend/src/backend/application/orders/subscript_to_order_updates.py
@@ -1,3 +1,7 @@
+from datetime import timedelta, timezone
+from dateutil.utils import today
+
+from backend.application.common.dto import InRangeFilter
 from backend.domain.value_objects import UserRole
 from src.backend.application.common.authorization import AccessTokenI
 from src.backend.application.common.interactor import Interactor
@@ -11,15 +15,19 @@ class GetOrdersListResultDTO(DomainModelBase):
     items: list[Order]
 
 
-# VALIDATE TOKEN IN DEPS
-class GetOrdersList(Interactor[None, GetOrdersListResultDTO]):
+class GetAllOrdersForTodayList(Interactor[None, GetOrdersListResultDTO]):
     def __init__(self, uow: UoW, token: AccessTokenI):
         self.uow = uow
         self.token = token
 
     async def __call__(self, data: None = None) -> GetOrdersListResultDTO:
-        in_range_filter = {}
+        if self.token.user_role == UserRole.client:
+            raise AuthorizationError("You have no right to access all orders list")
+        in_range_filter: InRangeFilter = {
+            "created_at": (today(timezone.utc), today(timezone.utc) + timedelta(days=1))
+        }
         async with self.uow:
-            orders_list = await self.uow.order.find_many(in_range_filter=in_range_filter)
+            orders_list = await self.uow.order.find_many(
+                in_range_filter=in_range_filter
+            )
             return GetOrdersListResultDTO(items=list(orders_list))
-
