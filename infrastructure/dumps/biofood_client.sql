7ec6388a4390eb0b82027ee3ea4b72eb5deef737
davikch <139548630+davikch@users.noreply.github.com>
2025-03-06 02:44:41 +1000

    🗃️  Added banner paths to dump (#59)
    Brought to you by impeccable manual cooperative dump editing

--- infrastructure/dumps/biofood_client.sql
+++ infrastructure/dumps/biofood_client.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f8c6e5e76d83a7c545ca6c7b68308c741165f495125f044c076470add17dc3a
-size 4245562
+oid sha256:909ad05b85bb2331c3b56237b74d4a7998ae22c9e6c197a7664fa3f0de9aea74
+size 4248336
